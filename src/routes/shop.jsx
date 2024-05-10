2ab86de0b94f66c94f4d3909e5edaf6dd579331c
Eliot Smith <eliot@weboffools.com>
2024-05-10 09:29:25 -0600

    Add add to cart form for each product

--- src/routes/shop.jsx
+++ src/routes/shop.jsx
@@ -1,9 +1,11 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
+import ProductCard from '../components/ProductCard';
+import styles from '../styles/shop.module.css';
 
 const useShopItems = () => {
-  const [shopItems, setShopItems] = useState(null);
+  const [shopItems, setShopItems] = useState([]);
   const [error, setError] = useState(null);
-  const [loading, setLoading ] = useState(true);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch(`${import.meta.env.VITE_SHOP_URL}`, { mode: 'cors' })
@@ -16,21 +18,24 @@ const useShopItems = () => {
       .then((json) => setShopItems(json))
       .catch((error) => setError(error))
       .finally(() => setLoading(false));
-  },[]);
+  }, []);
 
   return { shopItems, error, loading };
 };
 
 export default function Shop() {
   const { shopItems, error, loading } = useShopItems();
+  const products = shopItems.map((item) => (
+    <ProductCard key={item.id} item={item} />
+  ));
 
   if (error) return <p>{error.statusText || error.message}</p>;
   if (loading) return <p>Loading ...</p>;
 
   console.log(shopItems);
   return (
-    <div className='shop'>
-    <h1>This is the Shop page.</h1>
+    <div className="shop">
+      <div className={styles.productArea}>{products}</div>
     </div>
   );
 }
